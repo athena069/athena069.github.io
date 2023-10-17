9f2ed7325a9d5a7a540892a816adbd805af45270
athena <athena@athena.com>
2023-10-17 15:34:05 +0800

    测试5

--- static/views/download/download.js
+++ static/views/download/download.js
@@ -18,11 +18,9 @@ const downloadComponent = Vue.component("download-component", {
     const urlParams = new URLSearchParams(window.location.search);
 
     const channel = urlParams.get("channel");
-    console.log(channel);
     this.installParams.channel = channel
 
     const shareCode = urlParams.get("shareCode");
-    console.log(shareCode);
     this.installParams.code = shareCode
   },
   methods: {
@@ -42,7 +40,6 @@ const downloadComponent = Vue.component("download-component", {
       const copyContent = async () => {
         try {
           await navigator.clipboard.writeText(JSON.stringify(this.installParams));
-          console.log('clipboard successfully set')
         } catch (err) {
           console.error('Failed to copy: ', err);
         }
