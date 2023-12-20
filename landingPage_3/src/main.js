26625773b9b013472ddfb2df1e637a4d38be796f
athena <athena@athena.com>
2023-12-20 17:42:13 +0800

    Add Vue i18n and Vuetify dependencies

--- landingPage_3/src/main.js
+++ landingPage_3/src/main.js
@@ -1,11 +1,47 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 import router from './router'
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
+const datetimeFormats = {
+    'en-US': {
+        short: {
+            year: 'numeric', month: 'short', day: 'numeric'
+        },
+        long: {
+            year: 'numeric', month: 'short', day: 'numeric',
+            weekday: 'short', hour: 'numeric', minute: 'numeric'
+        }
+    },
+    'ja-JP': {
+        short: {
+            year: 'numeric', month: 'short', day: 'numeric'
+        },
+        long: {
+            year: 'numeric', month: 'short', day: 'numeric',
+            weekday: 'short', hour: 'numeric', minute: 'numeric', hour12: true
+        }
+    }
+}
+const i18n = createI18n({
+    datetimeFormats
+})
 const app = createApp(App)
 
-app.use(router)
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
+  
 
+app.use(router)
+app.use(i18n)
+app.use(vuetify)
 app.mount('#app')
